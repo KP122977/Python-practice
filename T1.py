db178056eb9459bb502ddf7b298fb078cfd741fb
KP122977 <krishc1201@gmail.com>
2025-12-09 13:53:16 +0530

    practice set first 4 que

---
+++ T1.py
@@ -0,0 +1,7 @@
+# Write a Python program to calculate the area of a rectangle given its length and width
+
+
+l=int(input("enter length of rectangle :"))
+w=int(input("enter width of rectangle :"))
+Area = l*w 
+print("Area of rectangle is : " , Area)
